5ca7e3f633735193aef4f5f1929e95463da26a08
suhanur-rohoman-sumon <sumonsuhanurrohoman@gmail.com>
2024-02-28 00:54:37 -0800

    comming soon added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Toaster } from "react-hot-toast";
 import Home from "./pages/home/Home";
 import Footer from "./pages/shered/footer/Footer";
+import ComingSoon from "./components/home/comingSoon/ComingSoon";
 
 const App = () => {
   return (
     <div className="overflow-hidden">
-      <Home />
+      {/* <Home />
       <Footer />
-      <Toaster />
+      <Toaster /> */}
+      <ComingSoon />
     </div>
   );
 };
